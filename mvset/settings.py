89a4ba719c7a7a3372dcf9c99bd8e81cc1a25caf
sandeepthoughtwin <sandeep.thoughtwin@gmail.com>
2022-06-14 16:03:21 +0530

    Modify Code

--- mvset/settings.py
+++ mvset/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
